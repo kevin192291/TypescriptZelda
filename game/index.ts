0777ecaab8b0a01ab0266b309974cfd0f8a288e4
Kevin Cantrell <kevin.cantrell@ert.com>
2018-08-14 13:05:10 -0400

    quick cleanup

--- game/index.ts
+++ game/index.ts
@@ -8,15 +8,13 @@ ex.Physics.enabled = true;
 ex.Physics.collisionResolutionStrategy = CollisionResolutionStrategy.Box;
 
 
-var map = LoadAllMaps(); // [new TiledResource('dist/assets/zelda.json')];
+var map = LoadAllMaps();
 var loader = new ex.Loader(map);
-var scenes: Scene[] = []; // = new ex.Scene(game);
 
 game.start(loader).then((success: any) => {
     map.forEach(map => {
         let scene = new ex.Scene(game);
         scene.addTileMap(map.getTileMap());
-        scenes.push(scene);
         game.addScene(map.name, scene);
     });
 
@@ -29,9 +27,7 @@ function LoadAllMaps() {
     const testFolder = './dist/assets/';
     const fs = require('fs');
     fs.readdirSync(testFolder).forEach((file: string) => {
-        debugger;
         if (file.includes('.json')) {
-            console.log(file);
             let mapObj = new TiledResource(testFolder + file);
             mapObj.name = file;
             maps.push(mapObj);
