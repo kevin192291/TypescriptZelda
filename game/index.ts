cea4cfde4785289b6431444eb5cf930dcdb9a772
Kevin Cantrell <kevin.cantrell@ert.com>
2018-08-29 14:57:20 -0400

    code cleanup

--- game/index.ts
+++ game/index.ts
@@ -1,40 +1,15 @@
 import * as ex from 'excalibur';
 import { CollisionResolutionStrategy } from 'excalibur';
-const admin = require('firebase-admin');
-
-admin.initializeApp({
-    apiKey: "AIzaSyAyx0wm5NyLsbuSJ8QlWZ6eFei6pfdE3mc",
-    authDomain: "communitygame-1febd.firebaseapp.com",
-    databaseURL: "https://communitygame-1febd.firebaseio.com",
-    projectId: "communitygame-1febd",
-    storageBucket: "",
-    messagingSenderId: "273246580010"
-});
-
 import { Player } from './actors/Player';
-import TiledResource from './maps/TiledResource';
-import { WeatherService } from './internet/weather/weatherService';
-import { IResources } from './models/resources.interface';
+import { LoadAllMaps, LoadAllSprites, LoadWeather, resources, loader } from './initialization';
 
 const game: ex.Engine = new ex.Engine({ displayMode: ex.DisplayMode.FullScreen });
-const loader: ex.Loader = new ex.Loader();
 ex.Physics.enabled = true;
 ex.Physics.collisionResolutionStrategy = CollisionResolutionStrategy.Box;
-var resources: IResources = {
-    maps: [],
-    sprites: [],
-    spriteSheets: [],
-};
 
 LoadAllMaps();
 LoadAllSprites();
 LoadWeather();
-
-
-
-//
-debugger;
-
 
 game.start(loader).then(() => {
     for (let map in resources.maps) {
@@ -50,34 +25,3 @@ game.start(loader).then(() => {
     game.goToScene('zelda');
     Player.create(game, game.currentScene, resources.spriteSheets['LinkSheet'], "kevin");
 });
-
-function LoadAllMaps(): void {
-    const testFolder = './dist/assets/';
-    const fs = require('fs');
-    fs.readdirSync(testFolder).forEach((file: string) => {
-        if (file.includes('.json')) {
-            const resourceName = file.split('.').slice(0, -1).join('.');
-            resources.maps[resourceName] = new TiledResource(testFolder + file);
-            loader.addResource(resources.maps[resourceName]);
-        }
-    });
-}
-
-function LoadAllSprites(): void {
-    const testFolder = './dist/assets/sprites/characters/';
-    const fs = require('fs');
-    fs.readdirSync(testFolder).forEach((file: string) => {
-        if (file.includes('.gif')) {
-            const resourceName = file.split('.').slice(0, -1).join('.');
-            resources.sprites[resourceName] = new ex.Texture(testFolder + file);
-            loader.addResource(resources.sprites[resourceName]);
-        }
-    });
-}
-
-function LoadWeather() {
-    let weatherService = new WeatherService();
-    weatherService.getWeather().then(t => {
-
-    });
-}
---
+++ game/initialization.ts
@@ -0,0 +1,51 @@
+import { IResources } from "./models/resources.interface";
+import { WeatherService } from "./internet/weather/weatherService";
+import ex = require("excalibur");
+import TiledResource from "./maps/TiledResource";
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    apiKey: "AIzaSyAyx0wm5NyLsbuSJ8QlWZ6eFei6pfdE3mc",
+    authDomain: "communitygame-1febd.firebaseapp.com",
+    databaseURL: "https://communitygame-1febd.firebaseio.com",
+    projectId: "communitygame-1febd",
+    storageBucket: "",
+    messagingSenderId: "273246580010"
+});
+export const loader: ex.Loader = new ex.Loader();
+export var resources: IResources = {
+    maps: [],
+    sprites: [],
+    spriteSheets: [],
+};
+
+export function LoadAllMaps(): void {
+    const testFolder = './dist/assets/';
+    const fs = require('fs');
+    fs.readdirSync(testFolder).forEach((file: string) => {
+        if (file.includes('.json')) {
+            const resourceName = file.split('.').slice(0, -1).join('.');
+            resources.maps[resourceName] = new TiledResource(testFolder + file);
+            loader.addResource(resources.maps[resourceName]);
+        }
+    });
+}
+
+export function LoadAllSprites(): void {
+    const testFolder = './dist/assets/sprites/characters/';
+    const fs = require('fs');
+    fs.readdirSync(testFolder).forEach((file: string) => {
+        if (file.includes('.gif')) {
+            const resourceName = file.split('.').slice(0, -1).join('.');
+            resources.sprites[resourceName] = new ex.Texture(testFolder + file);
+            loader.addResource(resources.sprites[resourceName]);
+        }
+    });
+}
+
+export function LoadWeather() {
+    let weatherService = new WeatherService();
+    weatherService.getWeather().then(t => {
+
+    });
+}
