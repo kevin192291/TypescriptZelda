f534f70eb83056ea318409766019cbad541d4efa
Kevin <Kevin903@live.com>
2018-10-09 19:36:53 -0400

    updated generator

--- game/maps/procedural/generator.ts
+++ game/maps/procedural/generator.ts
@@ -3,6 +3,10 @@ import { ITiledMap, ITiledMapLayer } from '../ITiledMap';
 export class MapGenerator {
     private tileMap: ITiledMap = null;
     private numberOfTiles: number = -1;
+    private tileWidth: number = 16;
+    private tileHeight: number = 16;
+    private mapWidth: number;
+    private mapHeight: number;
 
     constructor(
         mapWidth: number,
@@ -10,11 +14,16 @@ export class MapGenerator {
         tileWidth: number,
         tileHeight: number,
     ) {
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        this.mapWidth = mapWidth;
+        this.mapHeight = mapHeight;
         this.numberOfTiles = mapWidth * mapHeight;
-
+        
         this.tileMap = {
             name: this.guid(),
-            height: 10,
+            width: mapWidth,
+            height: mapHeight,
             layers: [
                 {
                     data: [1, 2, 48, 2, 45, 2, 51, 44, 2, 3, 11, 12, 12, 12, 55, 12, 61, 54, 24, 13, 11, 12, 12, 12, 12, 12, 12, 12, 34, 13, 11, 12, 12, 12, 12, 12, 12, 12, 68, 13, 11, 12, 12, 12, 12, 12, 12, 12, 12, 13, 11, 12, 12, 12, 12, 12, 12, 12, 12, 13, 11, 12, 12, 12, 12, 12, 4, 4, 4, 13, 11, 12, 12, 12, 12, 12, 4, 58, 4, 13, 11, 12, 12, 12, 12, 12, 4, 4, 4, 13, 21, 22, 22, 22, 64, 65, 22, 22, 22, 23],
@@ -31,7 +40,8 @@ export class MapGenerator {
             orientation: 'orthogonal',
             renderorder: 'right-down',
             tiledversion: '1.0.3',
-            tileheight: 16,
+            tilewidth: tileWidth,
+            tileheight: tileHeight,
             tilesets: [
                 {
                     columns: 10,
@@ -47,10 +57,8 @@ export class MapGenerator {
                     tilewidth: 16,
                     transparentcolor: '#ff00ff'
                 }],
-            tilewidth: 16,
             type: 'map',
             version: 1,
-            width: 10
         }
         this.generateLayerData();
     }
@@ -62,6 +70,7 @@ export class MapGenerator {
     private generateLayerData(): void {
         // This Deletes any pre-existing layers, this may change in the future
         // for hybrid maps
+        // TODO: Implement a 2nd layer for dark like night!
         this.tileMap.layers = [];
 
         const data: number[] = [];
@@ -71,8 +80,8 @@ export class MapGenerator {
         const layerNumber: number = this.tileMap.layers.length;
         const generatedLayer: ITiledMapLayer = {
             data: data,
-            width: 10,
-            height: 10,
+            width: this.mapWidth,
+            height: this.mapHeight,
             name: `Tile Layer ${layerNumber}`,
             opacity: 1,
             type: 'tilelayer',
