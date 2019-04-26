628bdf864c264c7c40558790b492c3644e56afc5
Kevin Cantrell <Kevin903@live.com>
2019-04-26 14:18:32 -0400

    assets will only be updated if they have changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const changed = require('gulp-changed');
+
 gulp.task('copy-assets', function () {
-	// Stuff here
-    console.log('About to copy files!');
-    gulp.src('./game/assets/sprites/characters/*.*').pipe(gulp.dest('dist/assets/sprites/characters'));
-    gulp.src('./game/assets/tilemaps/*.*').pipe(gulp.dest('dist/assets/tilemaps'));
-    gulp.src('./game/assets/tilesets/*.*').pipe(gulp.dest('dist/assets/tilesets'));
-    gulp.src('./game/assets/music/*.*').pipe(gulp.dest('dist/assets/music'));
-    return gulp.src('./game/assets/*.*').pipe(gulp.dest('dist/assets'));
+    gulp.src('./game/assets/sprites/characters/*.*').pipe(changed('dist/assets/sprites/characters'));
+    gulp.src('./game/assets/tilemaps/*.*').pipe(changed('dist/assets/tilemaps'));
+    gulp.src('./game/assets/tilesets/*.*').pipe(changed('dist/assets/tilesets'));
+    gulp.src('./game/assets/music/*.*').pipe(changed('dist/assets/music'));
+    return gulp.src('./game/assets/*.*').pipe(changed('dist/assets'));
 });
