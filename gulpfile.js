2af334b54cf89472d53058365bebfb54ca902f80
Kevin Cantrell <kevin903@live.com>
2021-08-28 23:30:00 +0900

    made lint happy

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,31 @@
 const gulp = require('gulp');
 const changed = require('gulp-changed');
 
-gulp.task('copy-assets', function() {
-    // Copy the Character sprites
-  gulp
-    .src('./game/assets/sprites/characters/*.*')
-    .pipe(changed('dist/assets/sprites/characters'))
-    .pipe(gulp.dest('dist/assets/sprites/characters'));
-    // Copy the tile maps
-  gulp
-    .src('./game/assets/tilemaps/*.*')
-    .pipe(changed('dist/assets/tilemaps'))
-    .pipe(gulp.dest('dist/assets/tilemaps'));
-    // copy the tilesets (pngs)
-  gulp
-    .src('./game/assets/tilesets/*.*')
-    .pipe(changed('dist/assets/tilesets'))
-    .pipe(gulp.dest('dist/assets/tilesets'));
-    // copy the audio
-  gulp
-    .src('./game/assets/music/*.*')
-    .pipe(changed('dist/assets/music'))
-    .pipe(gulp.dest('dist/assets/music'));
+gulp.task('copy-assets', () => {
+	// Copy the Character sprites
+	gulp.src('./game/assets/sprites/characters/*.*')
+		.pipe(changed('dist/assets/sprites/characters'))
+		.pipe(gulp.dest('dist/assets/sprites/characters'));
+	// Copy the tile maps
+	gulp.src('./game/assets/tilemaps/*.*')
+		.pipe(changed('dist/assets/tilemaps'))
+		.pipe(gulp.dest('dist/assets/tilemaps'));
+	// Copy the tilesets (pngs)
+	gulp.src('./game/assets/tilesets/*.*')
+		.pipe(changed('dist/assets/tilesets'))
+		.pipe(gulp.dest('dist/assets/tilesets'));
+	// Copy the audio
+	gulp.src('./game/assets/music/*.*')
+		.pipe(changed('dist/assets/music'))
+		.pipe(gulp.dest('dist/assets/music'));
 
-    gulp
-    .src('./game/assets/items/*.*')
-    .pipe(changed('dist/assets/items'))
-    .pipe(gulp.dest('dist/assets/items'));
+	gulp.src('./game/assets/items/*.*')
+		.pipe(changed('dist/assets/items'))
+		.pipe(gulp.dest('dist/assets/items'));
 
-    // Copy everything else in the asset directory
-  return gulp
-    .src('./game/assets/*.*')
-    .pipe(changed('dist/assets'))
-    .pipe(gulp.dest('dist/assets'));
+	// Copy everything else in the asset directory
+	return gulp
+		.src('./game/assets/*.*')
+		.pipe(changed('dist/assets'))
+		.pipe(gulp.dest('dist/assets'));
 });
--- index.js
+++ index.js
@@ -1,52 +1,53 @@
 'use strict';
 const electron = require('electron');
-const app = electron.app;
 
-// adds debug features like hotkeys for triggering dev tools and reload
+const {app} = electron;
+
+// Adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 
-// require('electron-reload')(__dirname, {
+// Fequire('electron-reload')(__dirname, {
 //   electron: require('electron-prebuilt')
 // });
 
-// prevent window being garbage collected
+// Prevent window being garbage collected
 let mainWindow;
 
 function onClosed() {
-  // dereference the window
-  // for multiple windows store them in an array
-  mainWindow = null;
+	// Dereference the window
+	// For multiple windows store them in an array
+	mainWindow = null;
 }
 
 function createMainWindow() {
-  const win = new electron.BrowserWindow({
-    width: 600,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      sandbox: false,
-    }
-  });
+	const win = new electron.BrowserWindow({
+		width: 600,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+			sandbox: false
+		}
+	});
 
-  win.loadURL(`file://${__dirname}/index.html`);
-  win.webContents.openDevTools();
-  win.on('closed', onClosed);
+	win.loadURL(`file://${__dirname}/index.html`);
+	win.webContents.openDevTools();
+	win.on('closed', onClosed);
 
-  return win;
+	return win;
 }
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-  if (!mainWindow) {
-    mainWindow = createMainWindow();
-  }
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
 });
 
 app.on('ready', () => {
-  mainWindow = createMainWindow();
+	mainWindow = createMainWindow();
 });
